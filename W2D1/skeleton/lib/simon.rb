0b3fe506709dccd0007cc0a87cf1a2325509ee3f
Kevin Bai <lkevin957@gmail.com>
2019-01-11 23:34:02 -0800

    Pass all simon says specs

--- W2D1/skeleton/lib/simon.rb
+++ W2D1/skeleton/lib/simon.rb
@@ -4,19 +4,30 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    until game_over 
+      take_turn
+    end 
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    unless @game_over
+      show_sequence
+      require_sequence  
+      round_success_message
+      @sequence_length += 1
+    end 
   end
 
   def show_sequence
-
+    add_random_color
   end
 
   def require_sequence
@@ -24,7 +35,7 @@ class Simon
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
@@ -36,6 +47,8 @@ class Simon
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
