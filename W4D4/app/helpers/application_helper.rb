6deda23f43a541efe06fb79b4c1a9730b1463dfd
Kevin Bai <lkevin957@gmail.com>
2019-01-31 10:13:03 -0800

    Write auto_gen filename method

--- W4D4/app/helpers/application_helper.rb
+++ W4D4/app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
         html += "value=\"#{form_authenticity_token}\">"
         html.html_safe
     end
+
+    def footer_gen
+        html = "<p>This is the #{caller[0][/[^:]+/][56...-9]} "
+        html += "cat place </p>"
+        html.html_safe
+  end
 end
