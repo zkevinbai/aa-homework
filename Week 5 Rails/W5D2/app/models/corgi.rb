113a8364c77ececb5511cedb269607d7c81d9a50
Kevin Bai <lkevin957@gmail.com>
2019-02-05 20:56:44 -0800

    Finish W5D3 hw

---
+++ Week 5 Rails/W5D2/app/models/corgi.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: corgis
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Corgi < ApplicationRecord
+    include Toyable
+    # has_many :toys, as: :imageable
+end
