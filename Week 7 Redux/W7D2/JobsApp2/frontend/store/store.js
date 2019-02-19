6ad793c4d97978f382bb3c9e61022537b5fe918f
Kevin Bai <lkevin957@gmail.com>
2019-02-18 21:09:27 -0800

    Finish W7D1 phase 2 and 3

--- Week 7 Redux/W7D2/JobsApp2/frontend/store/store.js
+++ Week 7 Redux/W7D2/JobsApp2/frontend/store/store.js
@@ -1,12 +1,30 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/root_reducer';
+import logger from 'redux-logger';
+
+function addLoggingToDispatch(store){
+  return function(next){
+    return function(action){
+        console.log("Action Received:", action);
+        console.log("State pre-dispatch:", store.getState());
+
+        let result = next(action);
+
+        console.log("State post-dispatch:", store.getState());
+
+        return result;
+    };
+  };
+}
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState);
+  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  console.log(applyMiddleware)
+  debugger
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
   return store;
-}
+};
 
 export default configureStore;
--- Week 7 Redux/W7D2/JobsApp2/frontend/todo_redux.jsx
+++ Week 7 Redux/W7D2/JobsApp2/frontend/todo_redux.jsx
@@ -4,23 +4,56 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
-const addLoggingToDispatch = store => next => action => {
-  console.log("Action Received:", action);
-  console.log("State pre-dispatch:", store.getState());
+// Stage 1
+// const addLoggingToDispatch = store => next => action => {
+//   console.log("Action Received:", action);
+//   console.log("State pre-dispatch:", store.getState());
 
-  let result = next(action);
+//   let result = next(action);
 
-  console.log("State post-dispatch:", store.getState());
+//   console.log("State post-dispatch:", store.getState());
 
-  return result;
-};
+//   return result;
+// };
+
+// Stage 2
+function addLoggingToDispatch(store){
+  return function(next){
+    return function(action){
+        console.log("Action Received:", action);
+        console.log("State pre-dispatch:", store.getState());
+
+        let result = next(action);
+
+        console.log("State post-dispatch:", store.getState());
+
+        return result;
+    };
+  };
+}
+
+// Stage 3
 
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
+  let store = configureStore(preloadedState);
   let dispatch = store.dispatch;
-  store.dispatch = addLoggingToDispatch(store)(dispatch);
+
+  // Stage 1
+  // store.dispatch = addLoggingToDispatch(store)(dispatch);
+
+  // Stage 2
+  // function applyMiddlewares(store, ...middlewares) {
+  //   middlewares = Array.from(middlewares);
+  //   middlewares.forEach((middleware) => {
+  //     dispatch = middleware(store)(dispatch);
+  //   }
+  //   );
+  //   return Object.assign({}, store, { dispatch });
+  // }
+
+  // store = applyMiddlewares(store, addLoggingToDispatch);
 
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
